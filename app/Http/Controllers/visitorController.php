4c504632f2bc9db9a003073246020d62c9c44fc0
EiadElrefaiy <EiadElrefaiy@gmail.com>
2023-08-25 21:11:35 +0300

    first commit

---
+++ app/Http/Controllers/visitorController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Visitor;
+use App\Models\User;
+
+class visitorController extends Controller
+{
+    public function recordVisit(Request $request){
+        $ip = $request->ip();
+        $visitor = Visitor::firstOrCreate(['ip' => $ip]);
+        $visitor->increment('visits');
+        $visitor->save();
+
+        $visitors = Visitor::count();
+
+        return view('visitorRecord', compact('visitors'));
+
+    }
+
+    public function readUser(string $id){
+        $ip = $request->ip();
+        $visitor = Visitor::firstOrCreate(['ip' => $ip]);
+        $visitor->increment('visits');
+        $visitor->save();
+
+        $visitors = Visitor::count();
+
+        return view('welcome', compact('visitors'));
+
+    }
+
+    public function register(){
+
+        return view('register');
+
+    }
+
+
+    public function save(Request $request){
+
+        //المفروض فيه validation
+
+        $name = $request -> name;
+        $email = $request -> email;
+        $password = $request -> password;
+
+
+        $user = User::create([
+         'name' => $name,
+         'email' => $email,
+         'password' => password_hash($password, PASSWORD_DEFAULT),
+        ]);
+
+        return view('success');
+    }
+}
